b6ea2c304bdf2c2f1479979746af992bbd81881b
Spencer Krull <spencerkrull138@gmail.com>
2022-07-09 18:57:31 -0600

    MoleContainer finished

---
+++ src/Components/EmptySlot.js
@@ -0,0 +1,14 @@
+// Just one example
+import Mole from './Mole'
+import MoleContainer from './MoleContainer'
+
+function EmptySlot(props){
+    return (
+        <div>
+            <h2> Empty Slot </h2>
+            <Mole />
+        </div>
+    )
+}
+
+export default EmptySlot
---
+++ src/Components/Mole.js
@@ -0,0 +1,13 @@
+// Just one example
+import MoleContainer from './MoleContainer'
+
+function Mole(props){
+    return (
+        <div>
+            <h2> Mole </h2>
+            <Mole />
+        </div>
+    )
+}
+
+export default Mole
